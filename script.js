8b663d1b32a59ae7b0a2fd08cae868e9bc4e54e6
Isaac Muniz <isaacdmuniz@yahoo.com>
2020-07-25 08:29:31 -0300

    Remastered! Added new script. Removed old script and index.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-document
-  .querySelector("#issue-input-form")
-  .addEventListener("submit", addIssue);
+const form = document.querySelector("#add-error-form");
+form.addEventListener("submit", addIssue);
 
 function addIssue(e) {
-  let issueDescription = document.querySelector("#issue-description-input")
-      .value,
-    issueSeverity = document.querySelector("#issue-severity-input").value,
-    issueAssign = document.querySelector("#issue-assign-input").value,
+  // get values from form
+  let issueTitle = document.getElementsByName("issue-title")[0].value,
+    issueDescription = document.getElementsByName("issue-description")[0].value,
+    issueSeverity = document.getElementsByName("issue-severity")[0].value,
+    issueAssign = document.getElementsByName("issue-assign")[0].value,
     issueId = chance.guid(),
     issueStatus = "Aberto";
 
+  // create issue obj
   let issue = {
+    title: issueTitle,
     description: issueDescription,
     severity: issueSeverity,
     assign: issueAssign,
@@ -18,97 +20,106 @@ function addIssue(e) {
     status: issueStatus,
   };
 
+  // save to the localStorage
   if (localStorage.getItem("issues") == null) {
-    // if 'issues' doesn't exist
+    // if the 'issues' item doesn't exists
     let issues = [];
     issues.push(issue);
     localStorage.setItem("issues", JSON.stringify(issues));
   } else {
-    // if it does exist
+    // if the 'issues' item already exists
     let issues = JSON.parse(localStorage.getItem("issues"));
     issues.push(issue);
     localStorage.setItem("issues", JSON.stringify(issues));
   }
 
-  document.querySelector("#issue-input-form").reset();
+  form.reset();
 
   fetchIssues();
 
   e.preventDefault();
 }
 
-function setStatus(id) {
-  const issues = JSON.parse(localStorage.getItem("issues"));
+function fetchIssues() {
+  // get the list of issues saved &
+  // the html element to display it
+  const issues = getData(),
+    issueList = document.querySelector("#issue-list");
 
-  // Set status to 'closed' when it is 'open',
-  // and to 'open' when it is 'closed'.
+  // clean the html before
+  issueList.innerHTML = "";
+
+  issues.forEach((issue) => {
+    let title = issue.title,
+      description = issue.description,
+      severity = issue.severity,
+      assign = issue.assign,
+      id = issue.id,
+      status = issue.status;
+
+    // insert the html
+    issueList.innerHTML +=
+      "<div class='issue" +
+      (issue.status == "Aberto" ? " issue-open'" : " issue-closed'") +
+      ">" +
+      "<div class='id'>ERRO: " +
+      id +
+      "</div>" +
+      "<div class='title-n-status'><div class='title'>" +
+      title +
+      "</div>" +
+      "<div class='status'>" +
+      status +
+      "</div></div><div class='descr'>" +
+      description +
+      "</div><div class='severity'>" +
+      "<i class='icon fas fa-exclamation-triangle'></i>" +
+      severity +
+      "</div><div class='assign'>" +
+      "<i class='icon fas fa-user-ninja'></i>" +
+      assign +
+      "</div><div class='buttons'><button onclick='setStatus(\"" +
+      id +
+      "\")'>" +
+      (issue.status == "Aberto" ? "Encerrar" : "Abrir") +
+      "</button><button onclick='deleteIssue(\"" +
+      id +
+      "\")'>Deletar</button></div></div>";
+  });
+}
+
+function setStatus(id) {
+  const issues = getData();
+
+  // change status
   issues.forEach((issue) => {
     if (issue.id == id) {
       issue.status = issue.status == "Aberto" ? "Encerrado" : "Aberto";
     }
   });
 
+  // save again
   localStorage.setItem("issues", JSON.stringify(issues));
 
   fetchIssues();
 }
 
 function deleteIssue(id) {
-  const issues = JSON.parse(localStorage.getItem("issues"));
+  const issues = getData();
 
+  // remove the issue with the given id
   for (let i = 0; i < issues.length; i++) {
     if (issues[i].id == id) {
       issues.splice(i, 1);
     }
   }
 
+  // save again
   localStorage.setItem("issues", JSON.stringify(issues));
 
   fetchIssues();
 }
 
-function fetchIssues() {
-  const issues = JSON.parse(localStorage.getItem("issues"));
-  const issueList = document.querySelector("#issueList");
-
-  issueList.innerHTML = "";
-
-  issues.forEach((issue) => {
-    let id = issue.id,
-      description = issue.description,
-      severity = issue.severity,
-      assign = issue.assign,
-      status = issue.status;
-
-    issueList.innerHTML +=
-      "<div class='well'>" +
-      "<h6>ID: " +
-      id +
-      "</h6>" +
-      "<p><span class='label label-info'>" +
-      status +
-      "</span></p>" +
-      "<h3>" +
-      description +
-      "</h3>" +
-      "<p><span class='glyphicon glyphicon-asterisk'></span> " +
-      severity +
-      "</p>" +
-      "<p><span class='glyphicon glyphicon-user'></span> " +
-      assign +
-      "</p>" +
-      // Change Status
-      (issue.status == "Aberto"
-        ? "<a href='#' onclick='setStatus(\"" +
-          id +
-          "\")' class='btn btn-warning' id='btn-status'>Encerrar</a> "
-        : "<a href='#' onclick='setStatus(\"" +
-          id +
-          "\")' class='btn btn-warning' id='btn-status'>Abrir</a> ") +
-      // Delete
-      "<a href='#' onclick='deleteIssue(\"" +
-      id +
-      "\")' class='btn btn-danger'>Deletar</a> " +
-      "</div>";
-  });
+function getData() {
+  return JSON.parse(localStorage.getItem("issues"));
 }
